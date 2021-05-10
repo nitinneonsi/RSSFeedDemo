b8ac9ef16588726f0e0f27ea0f80833be44e162e
Nitin <nitinneonsi@gmail.com>
2021-05-11 00:35:06 +0530

    Initial Commit

---
+++ RSSFeedDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RSSFeedDemo
+//
+//  Created by Nitz on 11/05/21.
+//  Copyright © 2021 Nihilent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
